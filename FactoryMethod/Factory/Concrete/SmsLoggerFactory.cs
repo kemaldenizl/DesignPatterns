f1d1423321f1e94eda2319bc05885fb1f5f991da
Kemal Deniz Lağap <kemaldenizlp@gmail.com>
2024-04-30 21:29:28 +0300

    [FactoryMethod] Creating Email and Sms logger's factory methods using ILoggerFactory

---
+++ FactoryMethod/Factory/Concrete/SmsLoggerFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryMethod.Factory.Abstract;
+using FactoryMethod.Loggers.Abstract;
+using FactoryMethod.Loggers.Concrete;
+
+namespace FactoryMethod.Factory.Concrete
+{
+    public class SmsLoggerFactory : ILoggerFactory
+    {
+        public ILogger Create()
+        {
+            return new SmsLogger();
+        }
+    }
+}
