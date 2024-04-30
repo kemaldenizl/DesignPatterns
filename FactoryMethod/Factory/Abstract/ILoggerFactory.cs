0cdad35dab7f005584b73ffa10ad76f00c6cc95b
Kemal Deniz Lağap <kemaldenizlp@gmail.com>
2024-04-30 21:24:26 +0300

    [FactoryMethod] Creating ILoggerFactory interface

---
+++ FactoryMethod/Factory/Abstract/ILoggerFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryMethod.Loggers.Abstract;
+
+namespace FactoryMethod.Factory.Abstract
+{
+    public interface ILoggerFactory
+    {
+        ILogger Create();
+    }
+}
