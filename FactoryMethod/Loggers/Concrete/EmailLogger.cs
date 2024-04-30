0d12988cbf6fbf6561d8dec9dbe9411a4de1f404
Kemal Deniz Lağap <kemaldenizlp@gmail.com>
2024-04-30 21:21:17 +0300

    [FactoryMethod] Creating Email and Sms logger.

---
+++ FactoryMethod/Loggers/Concrete/EmailLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryMethod.Loggers.Abstract;
+
+namespace FactoryMethod.Loggers.Concrete
+{
+    public class EmailLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to EMAIL!");
+        }
+    }
+}
