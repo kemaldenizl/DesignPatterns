7bf17642727597f2fd94503494b487a286d6630f
Kemal Deniz Lağap <kemaldenizlp@gmail.com>
2024-04-30 21:46:05 +0300

    [FactoryMethod] Testing factory method design pattern and finish.

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace FactoryMethod
+﻿using FactoryMethod.Factory.Concrete;
+
+namespace FactoryMethod
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            CustomerManager customerManager = new CustomerManager(new EmailLoggerFactory());
+            customerManager.CreateCustomer();
         }
     }
 }
