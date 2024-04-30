920fd9ee88ced3240232be1a5f58ce695c89d595
Kemal Deniz Lağap <kemaldenizlp@gmail.com>
2024-04-30 21:34:39 +0300

    [FactoryMethod] creating dependency injection for not depend a factory class

--- FactoryMethod/CustomerManager.cs
+++ FactoryMethod/CustomerManager.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FactoryMethod.Factory.Abstract;
 
 namespace FactoryMethod
 {
     public class CustomerManager
     {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
     }
 }
